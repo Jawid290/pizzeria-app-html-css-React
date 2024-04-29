c1d3d9de78b90b5827ef005a8f9b24e259ae763d
Jawid <Jawid.hashemi290@gmail.com>
2024-04-29 18:48:46 +0430

    Showing Hello world

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from "../assets/pizzaLogo.png";
+
+
+function Navbar() {
+  return (
+    <div>
+      Hello world!
+     
+    </div>
+  )
+}
+
+export default Navbar
